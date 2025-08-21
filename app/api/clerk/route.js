91f579bbd9ada996e16504b7bed0b294b6d420e1
Harsh70509 <hg70509@gmail.com>
2025-08-21 22:14:37 +0530

    updated headers in route js

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -2,13 +2,14 @@ import { Webhook } from "svix";
 import connectDB from "@/config/db";
 import User from "@/models/User";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import {  NextRequest } from "next/server";
 
 export async function POST(req) {
     const wh = new Webhook(process.env.SIGNING_SECRET);
     const headerPayload = await headers()
     const svixHeaders = {
         "svix-id": headerPayload.get("svix-id"),
+        "svix-timestamp": headerPayload.get("svix-timestamp"),
         "svix-signature": headerPayload.get("svix-signature"),
     };
     
@@ -43,5 +44,5 @@ export async function POST(req) {
             break;
     }
 
-    return NextResponse.json({message: "Event received"});
+    return NextRequest.json({message: "Event received"});
 }
